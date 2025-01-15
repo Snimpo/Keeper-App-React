5f47bee36393562bf5c19589860e43afbdb26d9f
Silvio <nirvash533@gmail.com>
2025-01-15 10:59:25 -0500

    Added buttons to add/remove notes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,37 @@
-import React from "react";
-import Header from "./Header.jsx";
-import Footer from "./Footer.jsx";
-import Note from "./Note.jsx";
-import "./App.css";
+import React, { useState } from 'react';
+import Header from './Header';
+import Note from './Note';
+import Footer from './Footer';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 function App() {
+
+  //useState and hooks to dynamically handle the addition or removal of the cards
+  const [notes, setNotes] = useState([]);
+
+  const addNote = () => {
+    setNotes([...notes, {}]);
+  };
+
+  const removeNote = (index) => {
+    setNotes(notes.filter((_, i) => i !== index));
+  };
+
   return (
-   
-      <div className="App">
-        <Header />
+    <div className="App">
+      <Header onAddNote={addNote} />
 
-        <Note />
-
-        <Footer />
+      <div className="Content" style={{ display: 'flex', flexDirection: 'row' }}>
+        {notes.map((note, index) => (
+          <Note key={index} onRemove={() => removeNote(index)} />
+        ))}
       </div>
-    
+
+      
+      
+
+      <Footer />
+    </div>
   );
 }
 
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-function Header() {
+function Header({onAddNote }) {
   return (
     //decided to use a header tag instead of a <h1>
     
       <header>
         <h1>Keeper App</h1>
+        <button className="add-note-button" onClick={onAddNote}>
+        Add Note
+      </button>
       </header>
     
   );
